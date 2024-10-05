bcfc8c5c5e68c4a60cfe2e83e487db3f957e6d62
daniyach <danielgonzalez6109@gmail.com>
2024-10-05 16:59:41 +0200

    Añadiendo la clase Celsius2Fahreinheit

--- ClaseUnidad2/src/claseDelDia2509/Celsius2Fahrenheit.java
+++ ClaseUnidad2/src/claseDelDia2509/Celsius2Fahrenheit.java
@@ -8,5 +8,21 @@ import java.util.Scanner;
 
 
 public class Celsius2Fahrenheit {
+	public static void main(String[] args) {
+		// Crear teclado 
+		Scanner tec = new Scanner(System.in);
+		
+		// Leer los grados en Celsius usando Scanner
+		System.out.print("Dame el número de grados Celsius que quieres transformar: ");
+		double gCelsius = tec.nextDouble();
+		tec.close();
+		
+		// Calcular e imprimir el equivalente en Fahrenheit
+		double gFahren = (gCelsius * 9 / 5) + 32 ;
+		
+		// Imprime los grados Fahrenheit
+		System.out.println("La equivalencia en Fahrenheit es: " + gFahren + "ºF");
+	}
+	
 	
 }
